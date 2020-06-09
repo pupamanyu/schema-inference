45da2390a0277eef612f876be8c0b9d726e4ee98
rajnish.malik <rajnish.malik@sunrun.com>
2020-06-08 18:22:08 -0700

    deploy & test changes

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,7 +3,7 @@ JAR_NAME=protoudf-0.0.1-jar-with-dependencies.jar
 GCS_JAR_ARTIFACT_BUCKET=gs://dataproc-temp-us-central1-21673414111-z46je66v/artifacts/scanseq
 GCS_BUCKET_DATA_OUT=gs://dataproc-temp-us-central1-21673414111-z46je66v/sampledata/out3
 REGION_NAME=us-central1
-CLUSTER_NAME=cluster-13f4
+CLUSTER_NAME=cluster-8c45
 
 mvn package -DskipTests=true
 
